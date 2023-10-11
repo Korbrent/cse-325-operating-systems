cde9dfbeb78e7afcf5fc3f9e5573012c2bd2cbbe
Kor <kortheshells@gmail.com>
2023-10-11 13:30:25 -0600

    oof

--- proc.h
+++ proc.h
@@ -50,7 +50,7 @@ struct proc {
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
   void *ustack;                // User stack for threads [CSE325 LAB 4]
-  int reference_count;         // Reference count for threads [CSE325 LAB 4]
+  int *reference_count;         // Reference count for threads [CSE325 LAB 4]
   int highPriorityTime;        // Added in
   int lowPriorityTime;         // Added in
   int mutex;
