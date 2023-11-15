bd92785243d65a042241921af8ba7fd533f806ec
Hunter Hancock <hancock.hunter@gmail.com>
2023-11-15 16:48:35 -0700

    last commit for lab5

--- testcow.c
+++ testcow.c
@@ -21,15 +21,40 @@ int main(int argc, char* argv[]) {
   if (id < 0) {
     printf(1, "%d failed in fork!\n", getpid());
     exit();
-    return;
+    return -1;
   }
 
+  char* anotherBigBlock = 0;
   if (id > 0) { // parent
     printf(1, "PARENT: # of free pages after fork: %d\n", getNumFreePages());
-    
+    anotherBigBlock = malloc(PGSIZE);
+    for (int i = 0; i < PGSIZE; i++) {
+      anotherBigBlock[i] = PGSIZE - i;
+    }
+    printf(1, "PARENT: # of free pages after construction: %d\n", getNumFreePages());
+    memmove((void*)(bigBlockOfMem), (void*)anotherBigBlock, PGSIZE / 2);
+    printf(1, "PARENT: # of free pages after copy: %d\n", getNumFreePages());
+    wait();
   } else { // child
     printf(1, "CHILD: # of free pages after fork: %d\n", getNumFreePages());
+    anotherBigBlock = malloc(PGSIZE);
+    for (int i = 0; i < PGSIZE; i++) {
+      anotherBigBlock[i] = i*2;
+    }
+    printf(1, "CHILD: # of free pages after construction: %d\n", getNumFreePages());
+    memmove((void*)(bigBlockOfMem+(PGSIZE/2)), (void*)anotherBigBlock, PGSIZE / 2);
+    printf(1, "CHILD: # of free pages after copy: %d\n", getNumFreePages());
+    exit();
+    return 0;
   }
 
+  // parent process only from this point
+  printf(1, "# of free pages after fork: %d\n", getNumFreePages());
+  printf(1, "first 10 values of block: [ ");
+  for (int i = 0; i < 10; i++) printf(1, "%d, ", bigBlockOfMem[i]);
+  printf(1, " ]\n");
+  printf(1, "last 10 values of block: [ ");
+  for (int i = 1; i <= 10; i++) printf(1, "%d, ", bigBlockOfMem[PGSIZE - i]);
+  printf(1, " ]\n");
   exit();
 }
